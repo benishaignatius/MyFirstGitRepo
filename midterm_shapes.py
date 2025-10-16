306c1eb0d4f49c14632b4cf3fa9437b67eacd6eb
Benisha Ignatius <2474672@edu.vaniercollege.qc.ca>
2025-10-16 15:34:08 -0400

    midterm_shapes

--- midterm_shapes.py
+++ midterm_shapes.py
@@ -10,6 +10,7 @@ import math
 #     return result.rstrip()
 # rectangle(5)
 
+###################################################
 
 # def triangle_rectangle(n):
 #     result = ""
@@ -22,14 +23,31 @@ import math
 #     return result.rstrip()
 # triangle_rectangle(10)
 
-def triangle(n):
-    result = ""
-    spaces = ""
-    for i in range (1, n+1):
-            spaces = " "
-            result += spaces +"*" * row + spaces + "\n"
+##################################################
 
+# def triangle(n):
+#     result = ""
+#     for i in range (1, n+1):
+#             result += " " * (n-i) + "*" * (2*i-1) + "\n"
+
+#     print(result)
+
+#     return result.rstrip()
+# triangle(8)
+
+###################################################
+
+def diamond(n):
+    result = ""
+    for i in range (1,n):
+         result += " " * (n-i) + "*" * (2*i-1) + "\n"
+
+    for i in range (n-1,0,-1): #start at n-1 uptil 0, with increments of -1
+         result += " " * (n-i) + "*" * (2*i-1) + "\n"
+         
     print(result)
 
     return result.rstrip()
-triangle(10)
+diamond(5)
+
+#different way of doing it
